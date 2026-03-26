c21471a4209cdfea44ed4fac23236c501ee4f273
reilchad60-eng <reilchad60@gmail.com>
2026-03-26 03:35:44 -0400

    Add proxy configuration for Binance exchange

---
+++ Crypt.c
@@ -0,0 +1,10 @@
+"exchange": {
+    "name": "binance",
+    "ccxt_config": {
+        "httpsProxy": "http://your-proxy-ip:port",
+        "wsProxy": "http://your-proxy-ip:port"
+    },
+    "ccxt_async_config": {
+        "httpsProxy": "http://your-proxy-ip:port"
+    }
+}
